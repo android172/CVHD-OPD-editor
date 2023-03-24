b2c6be8ad6ad07fb89f74564599825be651985db
android172 <63978434+android172@users.noreply.github.com>
2023-03-24 19:10:32 +0100

    Small code fix

--- src/main_window/main_window_image.cpp
+++ src/main_window/main_window_image.cpp
@@ -298,11 +298,7 @@ void MainWindow::import_image_as_sprite(
         _current_sprite_import_palette[i] = _current_image_palette.display[i];
 
     // Add layer to sprite view
-    ui->gv_sprite->add_sprite(
-        *_current_sprite_import,
-        _current_sprite_import_palette,
-        1.0f - ui->slider_transparency->value() / 100.0f
-    );
+    redraw_sprite();
 
     // Update UI
     ui->spin_sprite_pos_x->setValue(_current_sprite_import->x_pos);
